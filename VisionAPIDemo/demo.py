ad6416ce730e07e7364e44872ce932eeb480a109
scrntnstrnglr <bhattasi@tcd.ie>
2020-02-18 11:28:51 +0000

    ADded client and imageannotator

--- VisionAPIDemo/demo.py
+++ VisionAPIDemo/demo.py
@@ -1,3 +1,8 @@
 import os,io
 from google.cloud import vision
 from google.cloud.vision import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceACToken-InstrumentRec.json'
+client = vision.ImageAnnotatorClient()
+
+print(client)

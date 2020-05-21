cae03ce05f1657e6a7028a863bff144e4a125b07
scrntnstrnglr <bhattasi@tcd.ie>
2020-05-21 12:21:29 +0100

    Initial commit

--- VisionAPIDemo/demo.py
+++ VisionAPIDemo/demo.py
@@ -2,7 +2,7 @@ import os,io
 from google.cloud import vision
 from google.cloud.vision import types
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceACToken-InstrumentRec.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'leafy-acumen-268610-a38fd0012f87.json'
 client = vision.ImageAnnotatorClient()
 
 print(client)

7c7b3a3bea27fc20ef07ff6b1ac8a1ba3b1db3d4
scrntnstrnglr <bhattasi@tcd.ie>
2020-05-22 10:59:56 +0100

    add object detection function google api

--- VisionAPIDemo/demo.py
+++ VisionAPIDemo/demo.py
@@ -2,7 +2,12 @@ import os,io
 from google.cloud import vision
 from google.cloud.vision import types
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'leafy-acumen-268610-a38fd0012f87.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceACToken-InstrumentRec.json'
 client = vision.ImageAnnotatorClient()
-
 print(client)
+
+img_uri="https://cdnblog.picsart.com/2015/08/178443533002201.jpg"
+image = vision.types.Image()
+image.source.image_uri = img_uri
+response = client.text_detection(image=image)
+print(response)
